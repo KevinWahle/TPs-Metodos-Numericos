4e49f09fbc02682d69ae4cda15815009c7a3b95f
Nicolás <nbustelow@gmail.com>
2022-03-18 11:22:26 -0300

    minor comments

--- TP1/PuntoFlotante.py
+++ TP1/PuntoFlotante.py
@@ -1,17 +1,30 @@
+# ------------------------------------------------------------------------------
+#  @file     +Nombre del archivo (ej: template.c)+
+#  @brief    +Descripcion del archivo+
+#  @author   +Nombre del autor (ej: Salvador Allende)+
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# LIBRARIES
+# ------------------------------------------------------------------------------
 import math
 
+# ------------------------------------------------------------------------------
+# CLASSES
+# ------------------------------------------------------------------------------
 class binary16:
     def __init__(self, number):
         self.d = float(number)
         self.bits = [0]*16
         self.dec2bin()
+        # FOR DEBUG
         # print("Signo:", self.bits[0])
         # print("Exp:", self.bits[1:6])
         # print("Mantisa:", self.bits[6:16])
 
     def dec2bin(self):
-        ne = 5
-        nm = 10
+        ne = 5  # Cantidad de Bits de Exponente
+        nm = 10 # Cantidad de Bits de Mantisa
         self.bits[0]= 1 if self.d < 0 else 0    # Guardo el signo
         moduloD = abs(self.d)
         sesgo = 2**(ne-1)-1 
@@ -22,7 +35,7 @@ class binary16:
             return [0]*(ne+nm+1)
         
         if moduloD > (2-2**(-nm))*2**(2**ne-sesgo-2): # Si el numero es infinito
-            return self.bits[0] + [1]*ne + [0]*nm 
+            return self.bits[0] + [1]*ne + [0]*nm     #TODO: REVISAR PORQUE NO ANDA
 
         expTotal = math.floor(math.log2(moduloD))
         #TODO: que pasa si expTotal es menor a 2**(ne-1)
@@ -32,13 +45,15 @@ class binary16:
         mantisa = (moduloD/2**expTotal) - 1
         self.bits[6:16]=man2bin (mantisa, nm) # pasar y guardar mantisa en binario
 
-
     def identity(self):
         return self.bits
 
     def negative(self):
         return [1 if self.bits[0]==0 else 0] + self.bits[1:]
 
+# ------------------------------------------------------------------------------
+# FUNCTION DEF
+# ------------------------------------------------------------------------------
 def exp2bin (exp, expBits):
     cont = 0
     expb=[]
@@ -68,80 +83,22 @@ def man2bin (man, manBits):
 
     return manb
 
+def test(numb):
+    IeeeNumb = binary16(numb)
+    print('El número:', numb, '-> IEEE754: ', IeeeNumb.bits)
+    #print(IeeeNumb.bits)
+    #print(IeeeNumb.negative())
 
-pruebita = binary16(-0.2)
-print(pruebita.bits)
-print(pruebita.negative())
-
-
-
-
-
-
-
-
-
-
-
-# def binaryOfFraction(fraction):
- 
-
-#     binary = str()
-#     while (fraction):
-#         fraction *= 2
-
-#         if (fraction >= 1):
-#             int_part = 1
-#             fraction -= 1
-#         else:
-#             int_part = 0
-     
-#         binary += str(int_part)
- 
-#     return binary
- 
-
-# def floatingPoint(real_no):
- 
-
-#     sign_bit = 0
- 
-
-#     if(real_no < 0):
-#         sign_bit = 1
- 
-
-#     real_no = abs(real_no)
- 
-#     # Converting Integer Part
-#     # of Real no to Binary
-#     int_str = bin(int(real_no))[2 : ]
- 
-#     # Function call to convert
-#     # Fraction part of real no
-#     # to Binary.
-#     fraction_str = binaryOfFraction(real_no - int(real_no))
-#     ind = int_str.index('1')
-#     exp_str = bin((len(int_str) - ind - 1) + 127)[2 : ]
-#     mant_str = int_str[ind + 1 : ] + fraction_str
-#     mant_str = mant_str + ('0' * (23 - len(mant_str)))
- 
-#     return sign_bit, exp_str, mant_str
- 
-# # Driver Code
-# if __name__ == "__main__":
- 
-#     sign_bit, exp_str, mant_str = floatingPoint(-2.250000)
-#     ieee_32 = str(sign_bit) + '|' + exp_str + '|' + mant_str
-#     print("IEEE 754 representation of -2.250000 is :")
-#     print(ieee_32)
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+#                             MAIN (TestBench)
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+test(-2.4)
 
 
-
-
-
-
-
-
+# ------------------------------------------------------------------------------
+# Preguntas
+# ------------------------------------------------------------------------------
     #Suma/Resta puede ser en decimal?
     #Solo numeros normal o tambien subnormales?
