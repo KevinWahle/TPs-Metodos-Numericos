a1df5aef53e17e3b421074629a3cdf477a2dcc92
Nicolás <nbustelow@gmail.com>
2022-03-18 20:14:15 -0300

    programm work fine

--- TP1/PuntoFlotante.py
+++ TP1/PuntoFlotante.py
@@ -14,50 +14,117 @@ import math
 # ------------------------------------------------------------------------------
 class binary16:
     def __init__(self, number):
-        self.d = float(number)
+        self.d = 0
         self.bits = [0]*16
-        self.dec2bin()
+        self.extremo = self.dec2bin(number)  # Devuelve si el numero es un caso extremo y cual
+        self.bin2dec()
+
         # FOR DEBUG
         # print("Signo:", self.bits[0])
         # print("Exp:", self.bits[1:6])
         # print("Mantisa:", self.bits[6:16])
 
-    def dec2bin(self):
+    def dec2bin(self, number):
         ne = 5  # Cantidad de Bits de Exponente
         nm = 10 # Cantidad de Bits de Mantisa
-        self.bits[0] = 1 if self.d < 0 else 0    # Guardo el signo
-        moduloD = abs(self.d)
         sesgo = 2**(ne-1)-1 
         expTotal = 0   
 
-        # Si el numero esta en [-epsilon,+epsilon], lo consideramos 0
-        if moduloD < 2**(1-sesgo):  
-            return [0]*(ne+nm+1)
-        
+        if number != float('inf') and number != float('-inf') and not math.isnan(number):   # Si no es un caso extremo
+            self.bits[0] = 1 if number < 0 else 0   # Guardo el signo
+            modulo = abs(number)                    # y tomo su módulo
+
+        # Si el numero es NaN
+        if math.isnan(number):
+            self.bits = [0] + [1]*(ne+nm)
+            return "NaN"
+            
         # Si el numero es infinito
-        if moduloD > (2-2**(-nm))*2**(2**ne-sesgo-2):
-            self.bits[0] = 1 if self.d < 0 else 0    #TODO: No se porque lo tengo que repetir (Nico)  
-            for i in range(0, ne): 
-                self.bits[i+1] = 1  # Todo el exponente debe ser 1
-            return self.bits
+        if number == float('inf') or number == float('-inf') or modulo > (2-2**(-nm))*2**(2**ne-sesgo-2):
+            self.bits[0] = 1 if number < 0 else 0   # Guardo el signo
+            self.bits = [self.bits[0]] + [1]*ne + [0]*nm
+            return "+inf" if self.bits[0] == 0 else "-inf"
 
+        # Si el numero esta en [-epsilon,+epsilon], lo consideramos 0
+        if modulo < 2**(-nm)*2**(1-sesgo): 
+            self.bits = [0]*(ne+nm+1)
+            return "cero"
 
-        expTotal = math.floor(math.log2(moduloD))
-        #TODO: que pasa si expTotal es menor a 2**(ne-1)
-        exp = expTotal + sesgo        
-        self.bits[1:6]=exp2bin (exp, ne) # pasar y guardar exp en binario
+        # Subnormales
+        if modulo < 2**(1-sesgo):
+            expTotal=1-sesgo
+            self.bits[1:6]=[0]*5
+            mantisa=(modulo/2**expTotal)
+        else:
+            expTotal = math.floor(math.log2(modulo))
+            exp = expTotal + sesgo        
+            self.bits[1:6]=exp2bin (exp, ne) # pasar y guardar exp en binario
+            mantisa = (modulo/2**expTotal) - 1
 
-        mantisa = (moduloD/2**expTotal) - 1
         self.bits[6:16]=man2bin (mantisa, nm) # pasar y guardar mantisa en binario
+        return "NO"
 
-    def identity(self):
-        return self.bits
+    def bin2dec(self):
+        ne = 5  # Cantidad de Bits de Exponente
+        nm = 10 # Cantidad de Bits de Mantisa
+        sesgo = 2**(ne-1)-1
+        
+        if self.bits[1:] == [1]*ne + [0]*nm:        # Caso infinito
+            self.d= float('inf') if self.bits[0] == 0 else float('-inf')
+            return True
 
-    # Multiplicación por -1
-    def negative(self):
-        return [1 if self.bits[0]==0 else 0] + self.bits[1:]
+        elif self.bits[1:6] == [1]*ne:         # Caso NaN
+            self.d= float('NaN')
+            return True
 
-    #TODO: SUMA, RESTA y ¿MULTIPLICACION +1?
+        if self.bits[1:] == [0]*ne + [0]*nm:        # Caso 0
+            self.d = float(0)
+
+        elif self.bits[1:6] == [0]*ne:              # Caso Sub-Normal
+            mantis = 0
+            for i in range(nm):
+                mantis += self.bits[i+6]* 2**(-i-1)                
+            self.d = (-1)**self.bits[0]*mantis*2**(1-sesgo)
+
+        else:                                       # Caso Normal
+            mantis = 1
+            for i in range(nm):
+                mantis += self.bits[i+6] * 2**(-i-1)
+            expo = 0
+            for j in range(ne):
+                expo += self.bits[j+1] * 2**(ne-j-1)
+            self.d = (-1)**self.bits[0]*mantis*2**(expo-sesgo)    
+
+    def __pos__(self):
+        if self.d == float('-inf'):
+            return binary16(float('-inf'))
+        return binary16(self.d)
+        
+    # Multiplicación por -1   
+    def __neg__(self):
+        if self.d == float('inf'):
+            return binary16(float('-inf'))
+        return binary16(-self.d)
+    
+    # Suma
+    def __add__(self,other):    
+        d=self.d+other.d
+        return binary16(d)
+        
+    # Resta
+    def __sub__(self,other):
+        d=self.d-other.d
+        return binary16(d)
+
+    # In-Place Suma
+    def __iadd__(self,other):    
+        self = binary16(self.d+other.d)
+        return self
+        
+    # In-Place Resta
+    def __isub__(self,other):
+        self=binary16(self.d-other.d)
+        return self
 
 # ------------------------------------------------------------------------------
 # FUNCTION DEF
@@ -74,7 +141,6 @@ def exp2bin (exp, expBits):
         exp = exp // 2 
         cont += 1
     return expb    
-    # return exp2bin(exp//2) + [exp%2] if exp>1 else [exp]
 
 def man2bin (man, manBits):
     cont = 0
@@ -97,15 +163,79 @@ def man2bin (man, manBits):
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 def test(numb):
+    
     IeeeNumb = binary16(numb)
-    print('El número:', numb, '-> IEEE754: ', IeeeNumb.bits)
-    #print(IeeeNumb.negative())
+    IeeeNumb2 = binary16(numb*2)
+    res = binary16(0)
 
-while True:
-    print("Write a number:", end="")
-    var = input()
-    test(var)
+    print('Numero inicial:', numb)
 
+    print("SUMA")
+    res = IeeeNumb + IeeeNumb2
+    print('a+b: ',IeeeNumb.d, '+', IeeeNumb2.d,'=', res.d, '-> IEEE754: ', res.bits)
+
+    print("RESTA")
+    res = IeeeNumb - IeeeNumb2
+    print('a-b: ', IeeeNumb.d, '-', IeeeNumb2.d, '=', res.d, '-> IEEE754: ', res.bits)
+
+    print("SUMA2")
+    res =  IeeeNumb
+    res += IeeeNumb2
+    print('a+=b: ',IeeeNumb.d, '+=' , IeeeNumb2.d, '=', res.d,'-> IEEE754: ', res.bits)
+
+    print("RESTA2")
+    res =  IeeeNumb
+    res -= IeeeNumb2
+    print('a-=b: ',IeeeNumb.d, '-=' , IeeeNumb2.d, '=', res.d,'-> IEEE754: ', res.bits)
+
+    print("POS")
+    res = +IeeeNumb
+    print('+a: ', IeeeNumb.d, '=', res.d, '-> IEEE754: ', res.bits)
+
+    print("NEG")
+    res = -IeeeNumb
+    print('-a: ', '-', IeeeNumb.d , '=', res.d, '-> IEEE754: ', res.bits)
+
+
+# Numero positivo
+test(4)
+print("\n\n")
+
+# Numero negativo
+test(-4)
+print("\n\n")
+
+# Numero subnormal
+test(3e-7)
+print("\n\n")
+
+# Numero subnormal negativo
+test(-3e-7)
+print("\n\n")
+
+# Numero muy cercano a cero -> 0
+test(-5e-10)
+print("\n\n")
+
+# Numero mayor al mas grande -> inf
+test(999999999)
+print("\n\n")
+
+# Numero menor al mas chico -> -inf
+test(-999999999)
+print("\n\n")
+
+# Infinito de float
+test(float('+inf'))
+print("\n\n")
+
+# Infinito negativo de float
+test(float('-inf'))
+print("\n\n")
+
+# Not a number de float
+test(float('NaN'))
+print("\n\n")
 
 # ------------------------------------------------------------------------------
 # Preguntas
