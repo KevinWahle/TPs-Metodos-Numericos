776dd50db00d57a35824097e40ee67af7b35c46f
Nicolás <nbustelow@gmail.com>
2022-03-18 21:09:38 -0300

    final version 1

--- TP1/PuntoFlotante.py
+++ TP1/PuntoFlotante.py
@@ -16,85 +16,87 @@ class binary16:
     def __init__(self, number):
         self.d = 0
         self.bits = [0]*16
-        self.extremo = self.dec2bin(number)  # Devuelve si el numero es un caso extremo y cual
+        self.dec2bin(number)  # Devuelve si el numero es un caso extremo y cual
         self.bin2dec()
-
-        # FOR DEBUG
-        # print("Signo:", self.bits[0])
-        # print("Exp:", self.bits[1:6])
-        # print("Mantisa:", self.bits[6:16])
 
     def dec2bin(self, number):
         ne = 5  # Cantidad de Bits de Exponente
         nm = 10 # Cantidad de Bits de Mantisa
         sesgo = 2**(ne-1)-1 
-        expTotal = 0   
+        expTotal = 0 # Es a lo que se eleva el 2  
 
-        if number != float('inf') and number != float('-inf') and not math.isnan(number):   # Si no es un caso extremo
+        # Si no es un caso extremo
+        if number != float('inf') and number != float('-inf') and not math.isnan(number):   
             self.bits[0] = 1 if number < 0 else 0   # Guardo el signo
             modulo = abs(number)                    # y tomo su módulo
 
-        # Si el numero es NaN
-        if math.isnan(number):
+        # Si el número es NaN
+        if math.isnan(number):                      
             self.bits = [0] + [1]*(ne+nm)
-            return "NaN"
-            
-        # Si el numero es infinito
-        if number == float('inf') or number == float('-inf') or modulo > (2-2**(-nm))*2**(2**ne-sesgo-2):
-            self.bits[0] = 1 if number < 0 else 0   # Guardo el signo
-            self.bits = [self.bits[0]] + [1]*ne + [0]*nm
+            return True
+
+        # Si el número es infinito    
+        if number == float('inf') or number == float('-inf') or modulo > (2-2**(-nm))*2**(2**ne-sesgo-2):   
+            self.bits[0] = 1 if number < 0 else 0           # Guardo el signo
+            self.bits = [self.bits[0]] + [1]*ne + [0]*nm    # Coloco 1s en el exponente y 0s en la mantisa
             return "+inf" if self.bits[0] == 0 else "-inf"
 
-        # Si el numero esta en [-epsilon,+epsilon], lo consideramos 0
+        # Si el número es menor al  número subnormal mas pequeño, lo consideramos 0
         if modulo < 2**(-nm)*2**(1-sesgo): 
-            self.bits = [0]*(ne+nm+1)
-            return "cero"
+            self.bits = [0]*(ne+nm+1)       # Coloco 0s en el exponente y 0s en la mantisa
+            return True
 
-        # Subnormales
+        # Si el número es Sub-Normal
         if modulo < 2**(1-sesgo):
-            expTotal=1-sesgo
-            self.bits[1:6]=[0]*5
-            mantisa=(modulo/2**expTotal)
+            expTotal=1-sesgo                # Calculo el exponente (1 - sesgo)
+            self.bits[1:6]=[0]*ne           # Coloco 0s en el exponente
+            mantisa=(modulo/2**expTotal)    # Calculo la mantisa
+        # Si el número es Normal
         else:
-            expTotal = math.floor(math.log2(modulo))
-            exp = expTotal + sesgo        
-            self.bits[1:6]=exp2bin (exp, ne) # pasar y guardar exp en binario
-            mantisa = (modulo/2**expTotal) - 1
+            expTotal = math.floor(math.log2(modulo))    # Calculo el exponente total (e - sesgo)
+            exp = expTotal + sesgo                      # Calculo el exponente (e)
+            self.bits[1:6]=exp2bin (exp, ne)            # Guardo el exponente en binario
+            mantisa = (modulo/2**expTotal) - 1          # Calculo la mantisa
 
-        self.bits[6:16]=man2bin (mantisa, nm) # pasar y guardar mantisa en binario
-        return "NO"
+        self.bits[6:16]=man2bin (mantisa, nm)           # Guardo la mantisa en binario
 
     def bin2dec(self):
         ne = 5  # Cantidad de Bits de Exponente
         nm = 10 # Cantidad de Bits de Mantisa
         sesgo = 2**(ne-1)-1
         
-        if self.bits[1:] == [1]*ne + [0]*nm:        # Caso infinito
-            self.d= float('inf') if self.bits[0] == 0 else float('-inf')
+        # Caso infinito
+        if self.bits[1:] == [1]*ne + [0]*nm:        
+            self.d= float('inf') if self.bits[0] == 0 else float('-inf')    # Depenendiendo del signo el número sera -inf o +inf
             return True
 
-        elif self.bits[1:6] == [1]*ne:         # Caso NaN
+        # Caso NaN
+        elif self.bits[1:6] == [1]*ne:         
             self.d= float('NaN')
             return True
 
-        if self.bits[1:] == [0]*ne + [0]*nm:        # Caso 0
+        # Caso 0
+        if self.bits[1:] == [0]*ne + [0]*nm:        
             self.d = float(0)
 
-        elif self.bits[1:6] == [0]*ne:              # Caso Sub-Normal
+        # Caso Sub-Normal
+        elif self.bits[1:6] == [0]*ne:              
             mantis = 0
             for i in range(nm):
-                mantis += self.bits[i+6]* 2**(-i-1)                
-            self.d = (-1)**self.bits[0]*mantis*2**(1-sesgo)
+                mantis += self.bits[i+6]* 2**(-i-1)             # Calculo la mantisa en decimal         
+            self.d = (-1)**self.bits[0]*mantis*2**(1-sesgo)     # Armo mi número en decimal
 
-        else:                                       # Caso Normal
+        # Caso Normal
+        else:                                       
             mantis = 1
             for i in range(nm):
-                mantis += self.bits[i+6] * 2**(-i-1)
+                mantis += self.bits[i+6] * 2**(-i-1)            # Calculo la mantisa en decimal
             expo = 0
             for j in range(ne):
-                expo += self.bits[j+1] * 2**(ne-j-1)
-            self.d = (-1)**self.bits[0]*mantis*2**(expo-sesgo)    
+                expo += self.bits[j+1] * 2**(ne-j-1)            # Calculo el exponente en decimal
+            self.d = (-1)**self.bits[0]*mantis*2**(expo-sesgo)  # Armo mi número decimal  
 
+    # Multiplicación por +1
     def __pos__(self):
         if self.d == float('-inf'):
             return binary16(float('-inf'))
@@ -129,25 +131,25 @@ class binary16:
 # ------------------------------------------------------------------------------
 # FUNCTION DEF
 # ------------------------------------------------------------------------------
-def exp2bin (exp, expBits):
+def exp2bin (exp, expBits):     # Convierte un numero decimal en binario con potencias positivas
     cont = 0
     expb=[]
 
-    if exp > 2**expBits:    # Numero mayor de lo que puedo guardar
+    if exp > 2**expBits:        # Numero mayor de lo que puedo guardar
         return [1, 1, 1, 1, 1]
         
     while cont<expBits:
-        expb = [exp%2] + expb
+        expb = [exp%2] + expb   # Divido por 2 y me quedo con el resto
         exp = exp // 2 
         cont += 1
     return expb    
 
-def man2bin (man, manBits):
+def man2bin (man, manBits):     # Convierte un numero decimal en binario con potencias negativas
     cont = 0
     manb=[]
       
-    while cont<manBits:
-        man *= 2
+    while cont<manBits:         
+        man *= 2                # Multiplico al numero por 2, y dependiendo de si es mayor a 1 o no, armo el binario
         if man>=1:
             manb = manb + [1]
             man -= 1
@@ -163,10 +165,9 @@ def man2bin (man, manBits):
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
 def operationTest(numb):
-    
-    IeeeNumb = binary16(numb)
-    IeeeNumb2 = binary16(numb*2)
-    res = binary16(0)
+    IeeeNumb = binary16(numb)       # Tomo un número
+    IeeeNumb2 = binary16(numb*2)    # Tomo otro número
+    res = binary16(0)               # Aquí se almacenará el resultado
 
     print('Numero inicial:', numb)
 
@@ -199,43 +200,43 @@ def operationTest(numb):
 def test():
     # Numero positivo
     operationTest(4.7)
-    print("\n\n")
+    print("\n")
 
     # Numero negativo
     operationTest(-3.14)
-    print("\n\n")
+    print("\n")
 
     # Numero subnormal
     operationTest(3e-7)
-    print("\n\n")
+    print("\n")
 
     # Numero subnormal negativo
     operationTest(-3e-7)
-    print("\n\n")
+    print("\n")
 
     # Numero muy cercano a cero -> 0
     operationTest(-5e-10)
-    print("\n\n")
+    print("\n")
 
     # Numero mayor al mas grande -> inf
     operationTest(999999999)
-    print("\n\n")
+    print("\n")
 
     # Numero menor al mas chico -> -inf
     operationTest(-999999999)
-    print("\n\n")
+    print("\n")
 
     # Infinito de float
     operationTest(float('+inf'))
-    print("\n\n")
+    print("\n")
 
     # Infinito negativo de float
     operationTest(float('-inf'))
-    print("\n\n")
+    print("\n")
 
-    # Not a number de float
+    # Not a Number de float
     operationTest(float('NaN'))
-    print("\n\n")
+    print("\n")
 
 # ------------------------------------------------------------------------------
 # MAIN
