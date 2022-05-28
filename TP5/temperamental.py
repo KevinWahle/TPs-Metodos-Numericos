60d8fc23a1f338d6482113785d79823ee82fe330
Nicolás <nbustelow@gmail.com>
2022-05-28 17:44:15 -0300

    added TP5 files

---
+++ TP5/temperamental.py
@@ -0,0 +1,26 @@
+# ------------------------------------------------------------------------------
+#  @file     +temperamental.py+
+#  @brief    +Optimización+
+#  @author   +Grupo 4+
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# LIBRARIES
+# ------------------------------------------------------------------------------
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math as mt
+
+# ------------------------------------------------------------------------------
+# FUNCTION DEF
+# ------------------------------------------------------------------------------
+# Optimización basada el método de gradientes conjugados usando interpolación cuadrática
+def minimi(f, Df, x0, tol, maxiter):
+    print("Principal")
+
+def temperatura():
+    print("Usa el TXT")
+
+def test():
+    print("TEST")
